7f901078cac97bf10508c491c8a97dc18dcea74c
DewangS <dewang.shahu@gmail.com>
2015-02-27 10:31:32 +1100

    Updated code as per Liam's suggetions

--- prs2.rb
+++ prs2.rb
@@ -3,14 +3,16 @@
 # This is a classic Paper Rock Scissors game
 # Author  : Dewang Shahu
 # Date  : 25 Feb 2015
+# Modifications :   Date    By        Comment
+#                   27FEB   DShahu    Updated as per reviewer's comments.
 
 #loop until user selects to quit the game
 def display_message(message)
   puts message
 end
 
-computer_choices = ["P", "R", "S"]
-outcomes = {['P', 'R'] => "You picked Paper and computer picked Rock.\nPaper wraps Rock...you won!!",
+CHOICES = ["P", "R", "S"]
+OUTCOMES = {['P', 'R'] => "You picked Paper and computer picked Rock.\nPaper wraps Rock...you won!!",
             ['R', 'P'] => "You picked Rock and computer picked Paper\nPaper wraps Rock...Computer won",
             ['R', 'S'] => "You picked Rock and computer picked Scissors.\nRock smashes Scissors...You won!!",
             ['S', 'R'] => "You picked Scissors and computer picked Rock.\nRock smashes Scissors...computer won",
@@ -25,27 +27,27 @@ puts "Let's Play Paper Rock Scissors!"
 
 another_round = 'Y'
 
-begin
+while another_round =='Y'
   begin
     puts "Choose one (p/r/s)"
     player_choice = gets.chomp.upcase
-    if /[PRS]/.match(player_choice) == nil
+    if !CHOICES.include?(player_choice)
         puts "*** Invalid choice... Please enter p/r/s and try again.. ***"
     end
-  end until /[PRS]/.match(player_choice) != nil
+  end until CHOICES.include?(player_choice)
   
 
-  comupter_choice = computer_choices[rand(3)]
+  comupter_choice = CHOICES[rand(3)]
 
   choice_key = []
 
   choice_key << player_choice
   choice_key << comupter_choice
 
-  display_message(outcomes[choice_key])
+  display_message(OUTCOMES[choice_key])
 
   
   puts "Play again?(Y/N)"
   another_round = gets.chomp.upcase
     
-end while another_round == 'Y'
+end 
